dc562969e87dca71f51daea2f550bec14f4afd86
Vempari <Vempari_2@mail.ru>
2019-06-23 09:34:13 +0800

    1.3. JavaDoc. Code convention[#152186]

--- chapter_001/src/ru/job4j/Calculate/Calculate.java
+++ chapter_001/src/ru/job4j/Calculate/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j.Calculate;
 
+/**
+ * Calculate.
+ *
+ * @author Pavel Kozhukhovskii.
+ */
+
 public class Calculate {
+    /**
+     * Main.
+     * @param args - args.
+     */
     public static void main(String[] args) {
         System.out.println("Hello, job4j");
     }
