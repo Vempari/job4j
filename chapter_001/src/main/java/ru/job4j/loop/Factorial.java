d12f1473d478d8b06e90bec35df9a9b09d52b7e3
Vempari <Vempari_2@mail.ru>
2019-06-27 16:37:16 +0800

    5.2. "Создать программу, вычисляющую факториал"

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -6,7 +6,7 @@ public class Factorial {
         if (n == 0) {
             return 1;
         }
-        for (int i = 1; i <= n; i++){
+        for (int i = 1; i <= n; i++) {
             result *= i;
         }
         return result;
