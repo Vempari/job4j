38a6929501992f663aa87ad3f4652758cef0ed29
Vempari <Vempari_2@mail.ru>
2019-07-01 11:13:02 +0800

    6.7. "Квадратный массив заполнен тру или фолс."

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class MatrixCheck {
     public boolean mono(boolean[][] data) {
         for (int i = 1; i < data.length; i++) {
-            if (data[i][i] != data[i - 1][i - 1]) {
+            if (data[i][i] != data[i - 1][i - 1] || data[data.length - 1 - i][i] != data[data.length - i][i - 1]) {
                 return false;
             }
         }
