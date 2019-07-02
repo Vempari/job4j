b7902dee32a1d878f584872cb41d4aa0c9c07693
Vempari <Vempari_2@mail.ru>
2019-07-02 12:44:45 +0800

    Пазлы, проблема с mvn install -Dcheckstyle.skip=true -DskipTests.

---
+++ chapter_001/src/main/java/ru/job4j/main.java
@@ -0,0 +1,16 @@
+package ru.job4j;
+
+public class main {
+    public static void calc(int value) {
+        if (value != 0 && value / 0 >= 0) {
+            System.out.println("div by zero");
+        } else {
+            System.out.println("zero by zero");
+        }
+    }
+    public static void main(String[] args) {
+        calc(0);
+    }
+}
+
+
