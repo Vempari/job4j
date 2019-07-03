bb1ae102fbabeafe16b99748ee16058ff5982cb3
Vempari <Vempari_2@mail.ru>
2019-07-03 17:02:22 +0800

    1.2 Рефакторинг площадь треугольника

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -1,7 +1,15 @@
 package ru.job4j.condition;
 
 public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
 
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
     /**
      * Метод вычисления полупериметра по длинам сторон.
      *
@@ -46,11 +54,11 @@ public class Triangle {
      *
      * @return Вернуть площадь, если треугольник существует или -1.
      */
-    public double area(int x1, int y1, int x2, int y2, int x3, int y3) {
+    public double area() {
         double rsl = -1;
-        double a = new Point(x1, y1).distance(new Point(x3, y3));
-        double b = new Point(x2, y2).distance(new Point(x3, y3));
-        double c = new Point(x1, y1).distance(new Point(x2, y2));
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
         double p = period(a, b, c);
         if (this.exist(a, b, c)) {
             rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
