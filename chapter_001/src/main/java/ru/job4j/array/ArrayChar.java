9843d34f18aeaead99562e098c9641f2233eea22
Vempari <Vempari_2@mail.ru>
2019-07-03 10:09:04 +0800

    Фикс комментариев

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -13,7 +13,6 @@ public class ArrayChar {
     public boolean startsWith(String word, String prefix) {
         char[] pref = prefix.toCharArray();
         char[] wrd = word.toCharArray();
-        // проверить. что массив data имеет первые элементы одинаковые с value
         for (int i = 0; i < pref.length; i++) {
             if (pref[i] != wrd[i]) {
                 return false;
--- chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -3,7 +3,7 @@ package ru.job4j.condition;
 /**
  * @author Pavel Kozhukhovskii (Vempari_2@mail.ru)
  * @version $Id$
- * @since 0.1
+ * @since 0.2
  */
 public class DummyBot {
     /**
@@ -14,10 +14,8 @@ public class DummyBot {
     public String answer(String question) {
         String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         if ("Привет, Бот.".equals(question)) {
-            // заменить ... на правильный ответ rsl = "ответ по заданию".
             rsl = "Привет, умник.";
-        } else if ("Пока.".equals(question)) { // заменить ... на проверку, известен ли боту этот вопрос и он знает как на него ответить.
-            // заменить ... на правильный ответ rsl = "ответ по заданию".
+        } else if ("Пока.".equals(question)) {
             rsl = "До скорой встречи.";
         }
         return rsl;
