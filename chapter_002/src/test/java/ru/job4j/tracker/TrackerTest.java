dd41e04786c595a7e5a019b101f8d56a0e5d4983
Vempari <Vempari_2@mail.ru>
2019-07-07 13:02:42 +0800

    2.2.2. Реализовать класс трекер. - фикс

--- chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -14,20 +14,20 @@ public class TrackerTest {
         Item result = tracker.findById(item.getId());
         assertThat(result.getName(), is(item.getName()));
     }
-//    @Test
-//    public void testFindByName() {
-//        Tracker tracker = new Tracker();
-//        Item item = new Item("test1","testDescription", 123L);
-//        Item item1 = new Item("test1","testDescription1", 1234L);
-//        Item item2 = new Item("test2","testDescription2", 1235L);
-//        Item item3 = new Item("test1","testDescription3", 1236L);
-//        tracker.add(item);
-//        tracker.add(item1);
-//        tracker.add(item2);
-//        tracker.add(item3);
-//        Item[] result = tracker.findByName(item.getName());
-//        assertThat(result.length, is(3));
-//    }
+    @Test
+    public void testFindByName() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1","testDescription", 123L);
+        Item item1 = new Item("test1","testDescription1", 1234L);
+        Item item2 = new Item("test2","testDescription2", 1235L);
+        Item item3 = new Item("test1","testDescription3", 1236L);
+        tracker.add(item);
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        Item[] result = tracker.findByName(item.getName());
+        assertThat(result.length, is(3));
+    }
     @Test
     public void testFindAll() {
         Tracker tracker = new Tracker();
@@ -56,9 +56,8 @@ public class TrackerTest {
         tracker.add(previous);
         Item next = new Item("test2","testDescription2",1234L);
         tracker.add(next);
-        next.setId(previous.getId());
         tracker.delete(previous.getId());
-        assertThat(tracker.findById(next.getId()).getName(), is("test1"));
+        assertThat(tracker.findById(previous.getId()), is( (Item) null));
     }
 
 
