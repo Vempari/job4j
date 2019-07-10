9e340ffa28bfab5516087c91c381ee1e5d88298c
Vempari <Vempari_2@mail.ru>
2019-07-10 12:27:20 +0800

    2.4.3. Используя класс StubInput написать тесты проверяющие поведение пользователя[#152239] - фикс

--- chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -3,7 +3,8 @@ package ru.job4j.tracker;
 import java.util.Scanner;
 
 
-public class ConsoleInput {
+public class ConsoleInput implements Input {
+    @Override
     public String ask(String ask) {
         Scanner sc = new Scanner(System.in);
         System.out.println(ask);
