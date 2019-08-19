d50a76d0ec0919d5a77300256546c43086ea93bb
Vempari <Vempari_2@mail.ru>
2019-08-19 14:52:55 +0800

    1. Подсчет функции в диапазоне.[#152097]

---
+++ chapter_004/src/test/java/ru/job4j/lambda/DiapasonTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.lambda;
+import org.junit.Test;
+
+import java.util.*;
+import java.util.function.*;
+import java.lang.*;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class DiapasonTest {
+    List<Double> diapason(int start, int end, Function<Double, Double> func) {
+        List<Double> list = new ArrayList<>();
+        for (int index = start; index < end; index++) {
+            list.add(func.apply((double) index));
+        }
+        return list;
+    }
+
+    @Test
+    public void whenLinearFunctionThenLinearResults() {
+        DiapasonTest function = new DiapasonTest();
+        List<Double> result = function.diapason(5, 8, x -> 2 * x + 1);
+        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenSquareFunctionThenSquareResults() {
+        DiapasonTest function = new DiapasonTest();
+        List<Double> result = function.diapason(5, 8, x -> x * x);
+        List<Double> expected = Arrays.asList(25D, 36D, 49D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenLogarifmFunctionThenLogarifmResults() {
+        DiapasonTest function = new DiapasonTest();
+        List<Double> result = function.diapason(10, 13, x -> Math.rint(Math.log(x)));
+        List<Double> expected = Arrays.asList(2D, 2D, 2D);
+        assertThat(result, is(expected));
+    }
+}
